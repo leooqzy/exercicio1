c817dabbe6b841176f772e929c4bf96dc1174ad1
leoqzyy <lemes7848@gmail.com>
2025-02-07 10:02:28 -0300

    arquivo trabalho

---
+++ 2b.php
@@ -0,0 +1,20 @@
+<?php
+
+    $a = readline("digite o primeiro valor");
+    $b = readline("digite o segundo valor");
+    $c = readline("digite o terceiro valor");
+    $d = readline("digite o quarto valor");
+
+    $n1 = $a * 1;
+    $n2 = $b * 2;
+    $n3 = $c * 3;
+    $n4 = $d * 4;
+
+    $rest = $n1 + $n2 + $n3 + $n4;
+
+    $result = $rest / 10;
+
+
+    echo "O resultado total foi de: $result ";
+
+?>
