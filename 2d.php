64909470f879f1edc4d8daa062436c31ca251e5a
leoqzyy <lemes7848@gmail.com>
2025-02-07 11:52:09 -0300

    Novo exercicio

---
+++ 2d.php
@@ -0,0 +1,14 @@
+<?php
+
+    $salario = readline("DIgite o valor do salario mínimo: ");
+    $cem1 = $salario / 216; 
+    $qui = readline("Digite o tanto de quilowatts da sua residência: ");
+
+    
+
+
+
+
+
+
+?>
