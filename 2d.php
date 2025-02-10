ae264df778b174f3ce87c1e6f488df9c911e02db
leoqzyy <lemes7848@gmail.com>
2025-02-10 10:32:17 -0300

    sla

--- 2d.php
+++ 2d.php
@@ -2,7 +2,9 @@
 
     $salario = readline("DIgite o valor do salario mínimo: ");
     $cem1 = $salario / 216; 
-    $qui = readline("Digite o tanto de quilowatts da sua residência: ");
+    $qui = readline("Digite o tanto de quilowatts gasto na sua residência: ");
+
+    
 
 
     echo "Valor a ser pago sem desconto $semdesc: ";
