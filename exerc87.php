59ec72b10e61bbd6c31cf45dbdf12dcc34aff8c3
leoqzyy <lemes7848@gmail.com>
2025-02-11 11:11:16 -0300

    Exercicio udemy

---
+++ exerc87.php
@@ -0,0 +1,23 @@
+<?php
+
+    $pessoa1 = [
+        'nome' => 'julia',
+        'idade' => 18,
+
+    ];
+
+    $pessoa2 = [
+        'nome' => 'pedro',
+        'idade' => 15,
+        
+    ];
+
+    if($pessoa1['idade'] >= 18) {
+        echo "é maior de idade \n";
+    }
+        else {
+            echo "É menor de idade \n";
+        }
+
+
+?>
