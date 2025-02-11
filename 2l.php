649cbf892424e8d8760f466e432f90dd165ba371
leoqzyy <lemes7848@gmail.com>
2025-02-11 11:23:07 -0300

    Começo do exercicio L

---
+++ 2l.php
@@ -0,0 +1,11 @@
+<?php
+
+    $n = readline("Digite aqui o seu numero e ele sera invertido: ");
+
+    $ni = strrev($n);
+
+    $soma = $n + $ni;
+
+    echo "A soma do seu número invertido com o número digitado: $soma \n";
+
+?>
