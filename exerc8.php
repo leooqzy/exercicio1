494e4edf1bad22cab2d7363c5f250ae037c8ee59
leoqzyy <lemes7848@gmail.com>
2025-02-12 09:33:26 -0300

    Novos exercicios

---
+++ exerc8.php
@@ -0,0 +1,9 @@
+<?php
+
+    $a = 10;
+    $b = 'leonardo';
+    $c = true;
+
+    echo " $a\n $b \n $c \n";
+
+?>
---
+++ exerc9.php
@@ -0,0 +1,10 @@
+<?php
+
+    $a = 10;
+    $b = 15.5;
+
+    $result = $a + $b;
+
+    echo "O resultado desta soma é de: $result \n";
+
+?>
