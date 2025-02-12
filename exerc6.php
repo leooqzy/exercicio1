6449fbe233bb9d7db2477caf37231c380ef6e77e
leoqzyy <lemes7848@gmail.com>
2025-02-12 08:51:07 -0300

    Exercicios novos

---
+++ exerc6.php
@@ -0,0 +1,10 @@
+<?php
+
+    $arr = ['nome' => 'Leo', 'idade' => 18, 'olhos' => 'verdes'];
+
+    print_r($arr['nome']);
+    echo "\n";
+    echo $arr['idade'];
+    echo "\n";
+
+?>
---
+++ exerc7.php
@@ -0,0 +1,10 @@
+<?php
+
+    $arr = ['nome' => 'leo', 'idade' => 15];
+
+
+    if($arr['idade'] <= 18){
+       echo "ele é menor de idade";
+    }
+
+?>
