75d156a0f1907142824b8e2a368d133b52e2d4e4
leoqzyy <lemes7848@gmail.com>
2025-02-12 08:35:11 -0300

    Exercicios novos

---
+++ exerc4.php
@@ -0,0 +1,17 @@
+<?php
+
+    $a = 30;
+    $b = 40.5;
+    $c = 50.5;
+
+    if(is_float($a)){
+        echo "esse numero é float \n";
+    }
+    else {
+        echo "esse numero não é float \n";
+    }
+
+    echo "Esse número é float $b \n";
+    echo "Esse número é float $c \n";
+
+?>
---
+++ exerc5.php
@@ -0,0 +1,6 @@
+<?php
+
+    echo "aaaaaaaaaaaaaaaaaa \n";
+    echo 'aaaaaaaaaaaaaaaaaaaa ';
+
+?>
