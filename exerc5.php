7901390a1921447b56920258bfb6b3a84cccde97
lemes <lemes7848@gmail.com>
2025-02-15 22:07:29 -0300

    Exercicios atualizados

--- exerc5.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    echo "aaaaaaaaaaaaaaaaaa \n";
-    echo 'aaaaaaaaaaaaaaaaaaaa ';
-
-?>
--- exerc8.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $a = 10;
-    $b = 'leonardo';
-    $c = true;
-
-    echo " $a\n $b \n $c \n";
-
-?>
